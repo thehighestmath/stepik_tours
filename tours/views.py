e7da24a59054ec745864a512cc310053fe499e33
thehigestmath <kirill.lfybk.rh@gmail.com>
2020-11-27 22:11:51 +0300

    tour done

--- tours/views.py
+++ tours/views.py
@@ -1,14 +1,32 @@
 from django.shortcuts import render
+import tours.dataset as dataset
+from random import seed, randint
 
 
 # Create your views here.
 def main_view(request):
-    return render(request, 'tours/index.html')
+    tours = []
+    while len(tours) != 6:
+        index = randint(1, len(dataset.tours))
+        if dataset.tours[index] not in tours:
+            tours.append(dataset.tours[index])
+    return render(request, 'tours/index.html', {
+        'departures': dataset.departures,
+        'tours': tours,
+    })
 
 
 def departure_view(request, departure):
-    return render(request, 'tours/departure.html')
+    return render(request, 'tours/departure.html', {
+        'departures': dataset.departures
+    })
 
 
 def tour_view(request, id):
-    return render(request, 'tours/tour.html')
+    tour = dataset.tours.get(id)
+    print(tour)
+    tour['stars'] = '★' * int(tour['stars'])
+    return render(request, 'tours/tour.html', {
+        'departures': dataset.departures,
+        'tour': tour
+    })
